8b9c8220c897481892fee29193974ac1dc63b126
Grace Tian <gracetian4ever@gmail.com>
2020-06-09 23:04:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count do
       final<< src[row_index][element_index] if src[row_index][element_index].to_s == src[row_index][element_index]
-    p final.join(" ")
+    return final.join(" ")
       element_index += 1 
   end 
     row_index += 1
